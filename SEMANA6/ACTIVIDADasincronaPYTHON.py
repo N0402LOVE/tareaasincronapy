34abd9a9e8990b06a3351599c901a998740514f8
N0402LOVE <126518262+N0402LOVE@users.noreply.github.com>
2023-03-04 09:59:33 -0600

    Add files via upload

---
+++ SEMANA6/ACTIVIDADasincronaPYTHON.py
@@ -0,0 +1,48 @@
+print ("SUMA DE LAS VARIABLES")
+a= 25
+b= 30
+c= 5
+suma= a + b + c
+print ("La suma de las tres variables es: ", suma)
+print ("")
+print ("Resta de las variables")
+uno= 10
+dos= 8
+tres= 30
+cuatro= 20
+resta= uno - dos - tres - cuatro
+print ("La resta de las cuatro variables es: ", resta)
+print ("")
+print ("Multiplicacón de la variable A por la variable B")
+A= 8
+B= 6
+variableC= A * B
+print ("Nos da como restado la Variable C que es: ", variableC)
+print ("")
+resultadoSuma= variableC * b
+print ("La Variable C multiplicada por la segunda variable de la suma es: ", resultadoSuma)
+print ("")
+resultadoResta= variableC* dos
+print  ("La Variable C multiplicada por la segunda variable de la resta es: ", resultadoResta)
+print ("")
+
+print ("VARIABLES EXPONENCIALES")
+variableExponencial= A ** B
+print ("El resultado de A elevado a la B es: ", variableExponencial)
+print ("")
+print ("MÓDULO")
+uno
+resultadoMódulo= a % uno
+print ("El resultado del módulo es: ", resultadoMódulo)
+print ("")
+print ("DIVISIÓN")
+variableExponencial
+resultadoMódulo
+total= variableExponencial / resultadoMódulo
+print("El resultado de la División es: ", total)
+print("")
+print("DIVISIÓ ENTERA")
+totalDivEntera= variableExponencial // resultadoMódulo
+print ("El resultado de la división entera es: ", totalDivEntera)
+
+
